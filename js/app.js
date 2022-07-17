0265f1266b743b09b44cf11c61d58203059a59c7
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2022-07-17 00:16:24 -0700

    html, css, and js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable quotes */
 'use strict';
 
-let userName=prompt('Hi there! What\'s your name?');
+let score = 0;
+
+let userName=prompt("Hi there! What's your name?");
 // console.log('The user\'s name is: ' + userName);
 alert('Welcome to my page, ' + userName + '!');
 
@@ -9,6 +13,7 @@ let ans1=answer1.toLowerCase();
 
 if(ans1==='yes' || ans1==='yea' || ans1==='yep' || ans1==='yeah' || ans1==='yup' || ans1==='y'){
   // console.log('The user knows my name.');
+  score++;
   alert('Awesome!');
 }else if(ans1==='no' || ans1 ==='nope' || ans1 ==='nah' || ans1==='n'){
   // console.log('The user does not know my name.');
@@ -21,9 +26,10 @@ if(ans1==='yes' || ans1==='yea' || ans1==='yep' || ans1==='yeah' || ans1==='yup'
 let answer2=prompt('How many cats do I have?');
 let ans2=answer2.toLowerCase();
 
-if(ans2==2 || ans2==='two'){
+if(ans2==2 || ans2=='two'){
   // console.log('correct answer');
-  alert('That\'s correct!');
+  score++;
+  alert("That's correct!");
 }else{
   // console.log('incorrect answer');
   alert('Good guess '+userName+'! I actually have 2 cats!');
@@ -34,7 +40,8 @@ let ans3=answer3.toLowerCase();
 
 if(ans3==='istanbul' || ans3==='istanbul, turkey' || ans3=== 'turkey' || ans3==='turkiye'){
   // console.log('correct answer');
-  alert('That\'s correct!');
+  score++;
+  alert("That's correct!");
 }else{
   // console.log('incorrect answer');
   alert('I actually grew in Istanbul, Turkey');
@@ -45,7 +52,8 @@ let ans4=answer4.toLowerCase();
 
 if(ans4==='golden gardens' || ans4==='golden gardens beach' || ans4==='golden gardens park'){
   // console.log('correct answer');
-  alert('That\'s correct!');
+  score++;
+  alert("That's correct!");
 }else{
   // console.log('incorrect answer');
   alert('No, but I like that one too!');
@@ -63,5 +71,48 @@ if(ans5==='yes' || ans5==='yea' || ans5==='yep' || ans5==='yeah' || ans5==='yup'
   window.location = 'https://canvas.instructure.com/courses/4898184/assignments/syllabus';
 }else{
   // console.log('invalid answer');
-  alert('I dont understand that answer.');
+  alert("I don't understand that answer.");
 }
+
+// guessing game:
+
+
+for (let i=0; i<4; i++) {
+  let ans6 = prompt("Guess a number between 0-10");
+  if(i>=3){
+    alert("sorry, you ran out of chances.");
+    break;
+  }else if(ans6==2){
+    score++;
+    alert("correct!");
+    break;
+  }else if(ans6>2){
+    alert("too high. try again!");
+  }else if(ans6<2){
+    alert("too low. try again!");
+  }else{
+    alert('that is not a valid answer. try again!');
+  }
+}
+
+function question7(){
+  let favoriteThings = ['cats', 'beaches', 'sushi', 'swimming'];
+  for (let i = 0; i < 6; i++) {
+    let ans7 = prompt("Which of the following things do you think I like?: cats, cars, skiing, beaches, sushi, bacon, running, swimming, re-watching movies, beer");
+    if(i>=5){
+      alert('sorry, you ran out of chances.');
+      return;
+    }
+    for (let j=0; j<favoriteThings.length; j++){
+      if(ans7==favoriteThings[j]){
+        score++;
+        alert('correct!');
+        return;
+      }
+    }
+    alert('incorrect. try again.');
+  }
+}
+question7();
+
+alert('you answered ' + score + ' questions correctly!');
