b64ef371eaf559d43b1a22471083e9b6a8478f0b
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2022-07-23 23:02:28 -0700

    cleaned up the js code

--- js/app.js
+++ js/app.js
@@ -3,38 +3,21 @@
 'use strict';
 
 let score = 0;
+
 let userName=prompt("Hi there! What's your name?");
 
-function questionPrompt(answer,correct1, correct2){
-  let message = prompt( answer.toLowerCase());
-  if(message===correct1 || message===correct2){
-    alert("correct");
-    return;
-  } alert("incorrect");
-}
-questionPrompt("Do I have two cats?", "yes", "y");
-// first function done
-questionPrompt("Did I grow up in Seattle, WA?", "no", "n");
-//second function done
-questionPrompt("Is Green Lake my favorite beach?", "no", "n");
-//third function done
-
-function name(){
-
+function greeting(){
   // console.log('The user\'s name is: ' + userName);
   alert('Welcome to my page, ' + userName + '!');
 }
-name();
+greeting();
 
 function questionName(){
 
-
-  let answer1=prompt('Do you know my name? (y/n)');
-  let ans1=answer1.toLowerCase();
+  let ans1=prompt('Do you know my name? (y/n)').toLowerCase();
 
   if(ans1==='yes' || ans1==='yea' || ans1==='yep' || ans1==='yeah' || ans1==='yup' || ans1==='y'){
   // console.log('The user knows my name.');
-    score++;
     alert('Awesome!');
   }else if(ans1==='no' || ans1 ==='nope' || ans1 ==='nah' || ans1==='n'){
   // console.log('The user does not know my name.');
@@ -46,41 +29,17 @@ function questionName(){
 }
 questionName();
 
-let ans2=prompt('How many cats do I have?').toLowerCase();
-
-
-if(ans2==2 || ans2=='two'){
-  // console.log('correct answer');
-  score++;
-  alert("That's correct!");
-}else{
-  // console.log('incorrect answer');
-  alert('Good guess '+userName+'! I actually have 2 cats!');
+function questionPrompt(question, correct1, correct2, correct3){
+  let answer = prompt(question).toLowerCase();
+  if(answer===correct1 || answer===correct2 || answer===correct3){
+    alert("Correct!");
+    score++;
+    return;
+  } alert("Incorrect.");
 }
-
-let answer3=prompt('Where did I grow up?');
-let ans3=answer3.toLowerCase();
-
-if(ans3==='istanbul' || ans3==='istanbul, turkey' || ans3=== 'turkey' || ans3==='turkiye'){
-  // console.log('correct answer');
-  score++;
-  alert("That's correct!");
-}else{
-  // console.log('incorrect answer');
-  alert('I actually grew in Istanbul, Turkey');
-}
-
-let answer4=prompt('Guess my favorite beach in Seattle');
-let ans4=answer4.toLowerCase();
-
-if(ans4==='golden gardens' || ans4==='golden gardens beach' || ans4==='golden gardens park'){
-  // console.log('correct answer');
-  score++;
-  alert("That's correct!");
-}else{
-  // console.log('incorrect answer');
-  alert('No, but I like that one too!');
-}
+questionPrompt("How many cats do I have?", "two", "2", "meow meow");
+questionPrompt("Where did I grow up?", "istanbul", "turkey", "istanbul, turkey");
+questionPrompt("Guess my favorite beach in Seattle", "golden gardens", "golden gardens beach", "golden gardens park");
 
 function eject(){
   let answer5=prompt('Do you want to know more? (y/n)');
@@ -101,7 +60,7 @@ function eject(){
 eject();
 // guessing game:
 
-function guessinggame(){
+function guessNumber(){
 
   for (let i=0; i<4; i++) {
     let ans6 = prompt("Guess a number between 0-10");
@@ -121,7 +80,7 @@ function guessinggame(){
     }
   }
 }
-guessinggame();
+guessNumber();
 
 function question7(){
   let countries = ['united states', 'u.s.', 'us', 'u.s.a.', 'usa', 'england', 'u.k.', 'uk', 'united kingdom', 'turkey', 'turkiye'];
